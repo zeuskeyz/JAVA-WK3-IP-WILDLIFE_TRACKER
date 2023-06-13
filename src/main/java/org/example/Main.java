a800dae099969b20fe47f91caed17ae42ebd3323
zeus <zeuskeyz@gmail.com>
2023-06-13 22:29:07 +0300

    adds the landing page and the landing page route on the Main.java file

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,22 @@
 package org.example;
 
+import static spark.Spark.get;
+import spark.ModelAndView;
+import static spark.Spark.staticFileLocation;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+
+
+
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        staticFileLocation("/public");
+        HandlebarsTemplateEngine views = new HandlebarsTemplateEngine();
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        get("/", (request, response) -> new ModelAndView(new HashMap<>(), "landing-page.hbs"), views);
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
     }
 }
